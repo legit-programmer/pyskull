bf70986522fbd258bc6768fbf9119488321a1043
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-08-18 16:24:06 +0530

    feat: added automatic tabbing feature

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,23 +1,34 @@
-
 interface props {
     code: string;
     setCode: any;
-    
 }
 
-const Textarea = ({ setCode }: props) => {
-    
+const Textarea = ({ code, setCode }: props) => {
+    const checkAndAddTab = () => {
+        
+        if(code[code.length-2]===':'){
+            const temp = code + '\t';
+            console.log(temp);
+            setCode(temp);
+        }
+    };
     return (
         <>
             <div>
                 <textarea
                     onChange={(e) => setCode(e.target.value)}
+                    value={code}
                     className="font-code p-2 w-[80vw] h-[75vh] bg-[#171A2F] text-white text-xl"
                     cols={30}
                     rows={10}
+                    onKeyUp={(key) => {
+                        if (key.key === "Enter") {
+                            checkAndAddTab();
+                        }
+                    }}
+    
                 />
             </div>
-            
         </>
     );
 };
